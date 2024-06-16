e98024608534808be2da6c455c544ec4f427503c
Chris-V <null>
2024-06-16 20:11:21 +0000

    🎶 synced file(s) with kronostechnologies/standards

--- files/docker-sbom.sh
+++ files/docker-sbom.sh
@@ -8,7 +8,7 @@ set -eo pipefail
 
 DEFAULT_OUTPUT_FILE="./docker-sbom.json"
 # renovate: datasource=docker depName=aquasec/trivy
-TRIVY_VERSION="0.52.0"
+TRIVY_VERSION="0.52.2"
 
 usage() {
   echo "Usage: $0 [-o result-file] [image-to-scan]"
