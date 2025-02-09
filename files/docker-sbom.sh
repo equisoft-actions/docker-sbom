3cb52e1d9819bd19e3a019de69ef1e62138fae12
Christopher Viel <Chris-V@users.noreply.github.com>
2025-02-09 16:09:16 -0500

    🎶 synced file(s) with kronostechnologies/standards (#46)
    Co-authored-by: Chris-V <null>

--- files/docker-sbom.sh
+++ files/docker-sbom.sh
@@ -8,7 +8,7 @@ set -eo pipefail
 
 DEFAULT_OUTPUT_FILE="./docker-sbom.json"
 # renovate: datasource=docker depName=aquasec/trivy
-TRIVY_VERSION="0.59.0"
+TRIVY_VERSION="0.59.1"
 
 usage() {
   echo "Usage: $0 [-o result-file] [image-to-scan]"
